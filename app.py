a1dc1d46f841b629cbbb33eca022a14ab04c7655
tofu4956 <tofu4956@users.noreply.github.com>
2021-10-28 17:17:47 +0900

    init fastapi

---
+++ app.py
@@ -0,0 +1,13 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+api = FastAPI()
+
+@api.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(api, host="0.0.0.0", port=8000)
